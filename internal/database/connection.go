df7d8c9b293d69cedea5f51243528e0621473c95
Dmytro Mandziuk <dmytromk@gmail.com>
2024-05-19 22:46:48 +0300

    delete unnecessary checks

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -12,6 +12,5 @@ func OpenConnection() (*sql.DB, error) {
 		"password=%s port=5432 sslmode=disable",
 		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_DB"), os.Getenv("POSTGRES_PASSWORD"))
-	fmt.Println(datasource)
 	return sql.Open("postgres", datasource)
 }
