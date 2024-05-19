4484a5c9c566d74e8a481533c3d36b3c5ff84309
Dmytro Mandziuk <dmytromk@gmail.com>
2024-05-19 21:08:06 +0300

    add dummy scheduler

--- cmd/email-scheduler/main.go
+++ cmd/email-scheduler/main.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	_ "github.com/lib/pq"
+	"github.com/robfig/cron"
 )
 
 func main() {
+	utc, _ := time.LoadLocation("")
+	cronHandler := cron.NewWithLocation(utc)
+	cronHandler.AddFunc("* * * * * *", func() {
+		fmt.Println("crontab ping")
+	})
+	cronHandler.Start()
+	select {}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module currency_exchange_rate
 go 1.21
 
 require (
+ 	github.com/robfig/cron v1.2.0
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
